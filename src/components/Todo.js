200da20d6226767dee97cff6425c3270de8102b4
ins1d30ut <bmoses001@protonmail.com>
2021-07-10 19:55:57 -0500

    Commit add components folder

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {}
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
